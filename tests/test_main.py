2d85cf1ea20c931aa200cd2e02291fd372b4eeee
Areef <areef13sykes@gmail.com>
2026-08-30 01:33:27 +0530

    Mul added

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-from src.main import add,sub
+from src.main import add,sub,mul
 
 def test_add():
     assert add(2, 3) == 5
@@ -9,3 +9,8 @@ def test_sub():
     assert sub(5, 3) == 2
     assert sub(0, 0) == 0
     assert sub(-1, -1) == 0
+
+def test_mul():
+    assert mul(2, 3) == 6
+    assert mul(-1, 1) == -1
+    assert mul(0, 5) == 0
